27b1139c2af7e59ff1f760adf435c2a99564cb0d
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2023-08-18 06:56:34 -0700

    refactor

--- lib/Screens/HomeScreen/home_screen.dart
+++ lib/Screens/HomeScreen/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../core/constants/assets.dart';
-import '../../core/constants/colors.dart';
+import 'Components/bottom_ad.dart';
 import 'Components/category.dart';
 import 'Components/dashboard_icon_account_section.dart';
 import 'Components/quote_like_text.dart';
@@ -45,65 +44,5 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
     );
-  }
-}
-
-class BottomAD extends StatelessWidget {
-  const BottomAD({
-    super.key,
-    required this.size,
-  });
-
-  final Size size;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-        width: size.width,
-        child: Column(children: [
-          const SizedBox(
-            height: 10,
-          ),
-          Container(
-              padding: const EdgeInsets.all(10),
-              height: 100,
-              width: size.width,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: MyColors.ksecondaryColor),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    height: double.infinity,
-                    width: size.width / 3.4,
-                    decoration: BoxDecoration(
-                        image: const DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage(Assets.assetsImagesFreakshake)),
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.red),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text("chocolate milkshake ",
-                          style: TextStyle(fontSize: 18)),
-                      TextButton(
-                        style: TextButton.styleFrom(
-                            backgroundColor: MyColors.kPrimaryColor,
-                            foregroundColor: Colors.white),
-                        onPressed: () {},
-                        child: const Text("Get Now 75%"),
-                      )
-                    ],
-                  )
-                ],
-              ))
-        ]));
   }
 }
