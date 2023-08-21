36de19b21959dd9c200c05654192e45e291a026a
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2023-08-20 17:16:06 -0700

    change the font style

--- lib/core/Themes/theme.dart
+++ lib/core/Themes/theme.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../constants/colors.dart';
 
 class MyTheme {
   static get darkTheme => ThemeData(
+        fontFamily: GoogleFonts.lato.toString(),
         colorScheme: ColorScheme.dark(primary: MyColors.kPrimaryColor),
         useMaterial3: true,
       );
